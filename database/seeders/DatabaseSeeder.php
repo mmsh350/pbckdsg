5ee1a7e3c0e672c07426914baef5a9e4c9fc27d0
Muhammad S. Hamidu <mhamidu@swglobal.local>
2026-07-28 12:56:42 +0100

    chore: add DocumentSeeder and NewsSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,12 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => Hash::make('password'),
+        $this->call([
+            DocumentSeeder::class,
+            NewsSeeder::class,
         ]);
+
+        if (!User::where('email', 'test@example.com')->exists()) {
+            User::factory()->create([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+                'password' => Hash::make('password'),
+            ]);
+        }
     }
 }
